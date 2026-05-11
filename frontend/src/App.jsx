4fdfeba7e502c50adc31f6c32eca7a65f353ade7
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-05-11 07:45:41 +0530

    Continue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,53 @@
 import "./App.css"
-// import { Editor } from "@monaco-editor/react"
+import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo, useState, useEffect } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
+
 
 function App() {
+
+  const editorRef = useRef(null)
+  const [username, setUsername] = useState(() => {
+    return new URLSearchParams(window.location.search).get("username") || ""
+  })
+
+  const ydoc = useMemo(() => new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
+
+  const handleMount = (editor) => {
+    editorRef.current = editor
+  }
+
+  const handleJoin = (e) => {
+    e.preventDefault()
+    setUsername(e.target.username.value)
+    window.history.pushState({}, "", "?username=" + e.target.username.value)
+  }
+
+  useEffect(() => {
+    editor
+  })
+
+  if(!username){
+    return (
+      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center">
+        <form
+        onSubmit={handleJoin}
+        className="flex flex-col gap-4">
+          <input type="text"
+          placeholder="Enter your username"
+          className="p-2 rounded-lg bg-gray-800 text-white"
+          name="username"
+          />
+          <button className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold">
+            join
+          </button>
+        </form>
+      </main>
+    )
+  }
 
   return (
     <>
@@ -10,13 +56,14 @@ function App() {
 
         </aside>
 
-        <section className="w-3/4 bg-neutral-800 rounded-lg">
-          {/* <Editor 
+        <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+          <Editor 
           height="100%"
           defaultLanguage="javascript"
           defaultValue="// some comment"
           theme="vs-dark"
-          /> */}
+          onMount={handleMount}
+          />
         </section>
       </main>
     </>
